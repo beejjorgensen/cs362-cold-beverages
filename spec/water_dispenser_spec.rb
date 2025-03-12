88d7faa0146033418cfef3b609a4f7c408058b98
Austen Turbyne <austenturbyne@yahoo.com>
2025-03-11 20:43:20 -0700

    step 1,2 3 up to freezer done

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -1,5 +1,45 @@
 require_relative '../lib/water_dispenser'
 
+class MyVessel
+    attr_reader :volume
+  
+    def initialize
+      @volume = 10
+    end
+end
+
+class MyReservoir
+    attr_reader :capacity
+    attr_accessor :current_water_volume
+  
+    def initialize(capacity = 10, initial_water_volume = 0)
+      @capacity = capacity
+      @current_water_volume = initial_water_volume
+    end
+  
+    def empty?
+      current_water_volume == 0
+    end
+  
+    def fill
+      current_water_volume = capacity
+    end
+     
+    def drain(volume)
+        self.current_water_volume -= volume
+      end
+end
 describe 'A water dispenser' do
 
+    it 'has a reservoir' do
+        water = MyReservoir.new(10, 10)
+        waterdispenser = WaterDispenser.new(water)
+        expect(waterdispenser.reservoir).to eq(water)
+      end
+    it 'can be dispense' do
+        water = MyReservoir.new(10, 10)
+        waterdispenser = WaterDispenser.new(water)
+        vessel = MyVessel.new
+        expect(waterdispenser.dispense(vessel)).to eq(1)
+    end
 end
--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -1,5 +1,35 @@
 require_relative '../lib/water_reservoir'
 
+class MyVessel
+    attr_reader :volume
+  
+    def initialize
+      @volume = 9
+    end
+end
+
 describe 'A water reservoir' do
+
+    it 'capacity and current_water_volume' do
+        waterreservoir = WaterReservoir.new(10, 10)
+        expect(waterreservoir.capacity).to eq(10)
+        expect(waterreservoir.current_water_volume).to eq(10)
+      end
+
+      it 'is initially empty' do
+        waterreservoir = WaterReservoir.new
+        expect(waterreservoir).to be_empty
+      end
+
+      it 'can be filled' do 
+        waterreservoir = WaterReservoir.new
+        expect(waterreservoir.fill).to eq(10)
+      end
+
+      it 'can be drained' do
+        waterreservoir = WaterReservoir.new(10, 10)
+        vessel = MyVessel.new
+        expect(waterreservoir.drain(vessel.volume)).to eq(1)
+      end
 
 end
