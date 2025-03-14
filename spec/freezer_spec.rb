0bb80202298f3a62129cc8e7305d3b8811ebb3b9
Abbey Hankins <abbeyhankins57@gmail.com>
2025-03-14 16:53:22 -0700

    getting freezer class underr tests

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -2,4 +2,54 @@ require_relative '../lib/freezer'
 
 describe 'A freezer' do
 
-end
+    it 'has a capacity and a temperature' do 
+        freezer = Freezer.new(100)
+        expect(freezer.capacity).to eq(100)
+        expect(freezer.temperature).to eq(70)
+    end 
+
+    it 'is initally off' do
+        freezer = Freezer.new
+        expect(freezer.instance_variable_get(:@power)).to eq(:off)
+    end 
+    
+    it 'can be turned on' do
+        freezer = Freezer.new
+        freezer.turn_on
+        expect(freezer.instance_variable_get(:@power)).to eq(:on)
+    end
+
+    it 'can be turned off' do 
+        freezer = Freezer.new
+        freezer.turn_on
+        freezer.turn_off
+        expect(freezer.instance_variable_get(:@power)).to eq(:off)
+    end
+
+    it 'can add items' do 
+        freezer = Freezer.new
+        item = double('item', volume: 10)
+        freezer.add(item)
+        expect(freezer.instance_variable_get(:@contents)).to include(item)
+    end 
+
+    it 'shows remaing capacity' do 
+        freezer = Freezer.new(100)
+        item1 = double('item1', volume: 30)
+        item2 = double('item2', volume: 20)
+        freezer.add(item1)
+        freezer.add(item2)
+        expect(freezer.remaining_capacity).to eq(50)
+    end 
+
+    it 'sets the temp' do 
+        freezer = Freezer.new
+        freezer.set_level(5)
+        expect(freezer.temperature).to eq(20)
+    end 
+end 
+
+
+
+
+
--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -2,4 +2,28 @@ require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
 
+    it 'has a capacity and a water' do 
+        reservoir = WaterReservoir.new(20, 10)
+        expect(reservoir.capacity).to eq(20)
+        expect(reservoir.current_water_volume).to eq(10)
+    end
+
+    it 'is intially empty' do 
+        reservoir = WaterReservoir.new
+        expect(reservoir).to be_empty
+    end 
+
+    it 'is no longer empty if volume is more than 0' do 
+        reservoir = WaterReservoir.new(10,5)
+        expect(reservoir).to_not be_empty
+    end 
+
+    it 'can be drained' do
+        reservoir = WaterReservoir.new(20, 10)
+        reservoir.drain(5)
+        expect(reservoir.current_water_volume).to eq(5)
+    end 
+
+
+
 end
